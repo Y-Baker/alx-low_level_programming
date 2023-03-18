4a0cd8b98828066553176484dd465732947e3fd0
Y-Baker <yuossefahmedgaber@gmail.com>
2023-03-18 12:39:07 -0700

    Add File 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,33 +1,38 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible different combinations of three digits
- * Return: Always 0 (Success)
+ * main - 101-alx
+ *
+ * Return: Always 0
  */
+
 int main(void)
 {
-	int n, m, l;
+	int a = 48, b, c;
 
-	for (n = 48; n < 58; n++)
+	while (a <= 57)
 	{
-		for (m = 49; m < 58; m++)
+		b = 48;
+		while (b <= 57)
 		{
-			for (l = 50; l < 58; l++)
+			c = 48;
+			while (c <= 57)
 			{
-				if (l > m && m > n)
+				if (b > a && c > b)
 				{
-					putchar(n);
-					putchar(m);
-					putchar(l);
-					if (n != 55 || m != 56)
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a != 55 || b != 56 || c != 57)
 					{
 						putchar(',');
 						putchar(' ');
 					}
 				}
+				c++;
 			}
+			b++;
 		}
+		a++;
 	}
-	putchar('\n');
-	return (0);
 }
