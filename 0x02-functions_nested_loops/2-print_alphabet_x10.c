bf9f5eddffecde016090d537cbbcd09cc88bb73b
Y-Baker <yuossefahmedgaber@gmail.com>
2023-03-21 11:38:26 -0700

    Add Fun 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - print alphabet x10
+ *
+ * Return: print_alphabet_x10
+ */
+
+void print_alphabet_x10(void)
+{
+	int i, count = 10;
+
+	while (count--)
+	{
+		i = 97;
+		while (i <= 122)
+		{
+			putchar(i);
+			i++;
+		}
+		_putchar('\n');
+	}
+}
