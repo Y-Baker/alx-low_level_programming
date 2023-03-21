2e38112f6dab2721cd31916be8b0273da0e6def1
Y-Baker <yuossefahmedgaber@gmail.com>
2023-03-21 14:01:05 -0700

    Add Funs

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,4 +7,9 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int i);
 int print_last_digit(int n);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
 
