5b4ae4a5a7de91c09d3e55ddbc1aaa1a55ffd503
Y-Baker <yuossefahmedgaber@gmail.com>
2023-03-21 12:26:13 -0700

    Add Fun 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - check if is an alpha or not
+ * @c: is the char to check
+ * Return: 1 if it an alpha, otherwise 0
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
+}
