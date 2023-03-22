30d24a46ea72912121d09a20cc3513322d4c7505
Y-Baker <yuossefahmedgaber@gmail.com>
2023-03-22 11:13:45 -0700

    Add Fun 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - print time table
+ */
+
+void times_table(void)
+{
+	int col, row = 0, re;
+
+	while (row <= 9)
+	{
+		col = 0;
+		while (col <= 9)
+		{
+			re = row * col;
+			if (col == 0)
+				_putchar('0');
+			else
+			{
+				if (re < 10)
+				{
+					printf("  %d", re);
+				}
+				else
+				{
+					printf(" %d", re);
+				}
+			}
+			if (col != 9)
+			{
+				_putchar(',');
+			}
+			col++;
+		}
+		putchar('\n');
+		row++;
+	}
+}
