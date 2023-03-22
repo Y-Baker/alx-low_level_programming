9e40e70758f49a068a9e7b71d64b864f231fc20a
Y-Baker <yuossefahmedgaber@gmail.com>
2023-03-22 11:35:41 -0700

    Add Fun 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,18 +1,13 @@
 #include "main.h"
 
 /**
- * add - adds two integers and returns the result
- * @a: int to be added to b
- * @b: int to be added to a
- * Return: sum of a and b
+ * add - add two numbers
+ * @a: the first number
+ * @b: the seconf number
+ * Return: the sum of a , b
  */
-	int add(int a, int b)
-	{
-		int c;
 
-
-		c = a + b;
-
-
-		return (c);
-	}
+int add(int a, int b)
+{
+	return (a + b);
+}
