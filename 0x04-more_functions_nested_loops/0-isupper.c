d5f5dd8f86faa634a0c3cd1a9f655ff087688021
Y-Baker <yuossefahmedgaber@gmail.com>
2023-03-23 17:39:11 -0700

    Add fun 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - check if it upper
+ * @c: the char
+ * Return: the answer
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
