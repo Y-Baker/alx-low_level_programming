2aafb749cc68d4dc0ad51c5100dce9b80d1ab3ab
Youssef Ahmed <115345602+Y-Baker@users.noreply.github.com>
2023-03-30 13:06:47 +0200

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
- * _strlen - fun 2
- * @s: the string
- * Return: the len of the string
+ * _strlen - print the length of a string
+ *
+ * Description: a function that returns the length of a string.
+ *
+ * @s: string variable
+ *
+ * Return: nothing
+ *
  */
 
 int _strlen(char *s)
 {
-	int count = 0;
+	int length = 0;
 
-	for (int i = 0; ; i++)
-	{
-		if (*(s + i) == '\00')
-			break;
-		count++;
-	}
-	return (count);
+	while (s[length])
+		length++;
+	return (length);
 }
