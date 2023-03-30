b1fcbd4a24679a1db2fdb6e830a89127f6a8e447
Youssef Ahmed <115345602+Y-Baker@users.noreply.github.com>
2023-03-30 13:34:12 +0200

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,22 @@
 #include "main.h"
 
 /**
- * puts2 - the fun 6
- * @str: the input string
+ * puts2 - prints xters of a string
+ * @str: the used string reference
+ * Return: 0
  */
 
 void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i])
+	while (str[i] != '\0')
 	{
 		if (i % 2 == 0)
+		{
 			_putchar(str[i]);
+		}
 		i++;
 	}
-	putchar('\n');
+	_putchar('\n');
 }
