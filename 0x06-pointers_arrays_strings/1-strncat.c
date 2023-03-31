661a67189b8a79a1057ca2d4367836412593f4ad
Y-Baker <yuossefahmedgaber@gmail.com>
2023-03-30 17:59:30 -0700

    Add Folder 6

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings,
+ * @dest: destination path.
+ * @src: source.
+ * @n: amount of bytes from src.
+ * Return: the pointer to destination.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int count = 0, count2 = 0;
+
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
+
+	while (count2 < n)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
+	return (dest);
+}
