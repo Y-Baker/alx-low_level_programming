f3c3bad55b17140e9e0bde95ccbdcd29842e5c41
Y-Baker <yuossefahmedgaber@gmail.com>
2023-04-03 06:17:38 -0700

    Add Fun 2, 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare two strings
+ * @s1: the first string
+ * @s2: the second string
+ * Return: the result
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] || s2[i])
+	{
+		if (s1[i] != s2[i])
+		{
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
