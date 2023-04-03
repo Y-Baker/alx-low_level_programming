8acea0682ffb154ce154ff56af64ad823e60ba09
Y-Baker <yuossefahmedgaber@gmail.com>
2023-04-03 13:54:56 -0700

    Add fun 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *leet - fun 7
+ * @a: the input string
+ * Return: the result string
+ */
+
+char *leet(char *a)
+{
+	int i = 0, n = 0;
+	char leet[] = "a4A4e3E3o0O0t7T7l1L1"
+
+	while (a[i])
+	{
+		n = 0;
+		while (leet[n] != 'L')
+		{
+			if (a[i] == leet[n])
+			{
+				a[i] = leet[n + 1];
+				break;
+			}
+			n += 2;
+		}
+		i++;
+	}
+	return (a);
+}
