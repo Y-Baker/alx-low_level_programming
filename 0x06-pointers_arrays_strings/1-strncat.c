4bab4b3844a38ccbdcb3b19ad212ef501548055a
Y-Baker <yuossefahmedgaber@gmail.com>
2023-04-03 05:43:46 -0700

    Add Fun 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *_strncat - Fun 1
+ * @src: the second string
+ * @dest: the first string
+ * @n: the lenth
+ * Return: the compine of the two strings
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len, len1 = 0, len2 = 0, i = 0;
+
+	while (dest[len1])
+	{
+		len1++;
+	}
+	while (src[len2])
+	{
+		len2++;
+	}
+	while (n-- && src[i] != '\0')
+	{
+		dest[len1] = src[i];
+		len1++;
+		i++;
+	}
+	return (dest);
+}
