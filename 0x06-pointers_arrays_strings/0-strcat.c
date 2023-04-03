57a212b3aa47ae49604fff18cbc4073fe1612355
Y-Baker <yuossefahmedgaber@gmail.com>
2023-04-03 05:31:27 -0700

    Add Fun 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,29 +1,30 @@
 #include "main.h"
 
 /**
- * _strcat - concatenates two strings
- * @dest: input value
- * @src: input value
- * Return: void
+ * *_strcat - Fun 1
+ * @src: the second string
+ * @dest: the first string
+ * Return: the compine of the two strings
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int len, len1 = 0, len2 = 0, i = 0;
 
-	i = 0;
-	while (dest[i] != '\0')
+	while (dest[len1])
 	{
+		len1++;
+	}
+	while (src[len2])
+	{
+		len2++;
+	}
+	len = len1 + len2 - 1;
+	while (len--)
+	{
+		dest[len1] = src[i];
+		len1++;
 		i++;
 	}
-	j = 0;
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	dest[i] = '\0';
 	return (dest);
 }
