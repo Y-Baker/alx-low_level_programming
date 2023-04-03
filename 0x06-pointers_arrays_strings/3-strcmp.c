fe13b000862493720c0f6f376315b6f7dd7f2f30
Y-Baker <yuossefahmedgaber@gmail.com>
2023-04-03 06:27:50 -0700

    Fix Fun 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcmp - compare two strings
+ * strcmp - compare two strings
  * @s1: the first string
  * @s2: the second string
  * Return: the result
@@ -9,15 +9,17 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
+	int i = 0, diff = 0;
 
 	while (s1[i] || s2[i])
 	{
 		if (s1[i] != s2[i])
 		{
-			return (1);
+			diff = *(s1 + i) - *(s2 + i);
+			if (diff != 0)
+				break;
 		}
 		i++;
 	}
-	return (0);
+	return (diff);
 }
