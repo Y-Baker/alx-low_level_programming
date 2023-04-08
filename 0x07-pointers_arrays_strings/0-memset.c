80513a4d198b866af5dee2fb0f05a96a6486eaf3
Y-Baker <yuossefahmedgaber@gmail.com>
2023-04-08 15:19:03 -0700

    Add Fun 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
- * _memset - fill a block of memory with a specific value
- * @s: starting address of memory to be filled
- * @b: the desired value
- * @n: number of bytes to be changed
- *
- * Return: changed array with new value for n bytes
- */
+ * _memset - fun 0
+ * @s: the start of memory
+ * @b: the char will be filled
+ * @n: the size
+ * Return: the new string
+*/
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
 
-	for (; n > 0; i++)
+	while (i < n)
 	{
 		s[i] = b;
-		n--;
+		i++;
 	}
 	return (s);
 }
