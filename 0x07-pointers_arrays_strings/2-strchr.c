622be70ad28cef2f55c7ab82bf849d9012574973
Youssef Ahmed <115345602+Y-Baker@users.noreply.github.com>
2023-04-09 13:08:22 +0200

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,23 +1,20 @@
 #include "main.h"
-/**
- * _strchr - the fun 2
- * @s: the string
- * @c: the char you look for
- * Return: the remain string after c
-*/
 
+/**
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
+ */
 
 char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (s[i])
+	for (; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
-		{
-			return (s + i);
-		}
-		i++;
+			return (&s[i]);
 	}
-	return (NULL);
+	return (0);
 }
