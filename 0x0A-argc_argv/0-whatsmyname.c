9b7d5bb7a4cb2b58d70981f20d9306c6433ba273
Y-Baker <yuossefahmedgaber@gmail.com>
2023-04-13 08:15:37 -0700

    Add Script 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,13 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * main - Prints the name of the program
+ * main - write the program name
  * @argc: Number of command line arguments
- * @argv: Array name
- * Return: 0
- */
+ * @argv: Array of arguments
+*/
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-	printf("%s\n", argv[argc * 0]);
-
+	printf("%s\n", argv[0]);
 	return (0);
 }
