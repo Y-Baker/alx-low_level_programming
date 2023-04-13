0b3d36e823fcfe89fbb37ce4d8d2afbd6dfb5502
Y-Baker <yuossefahmedgaber@gmail.com>
2023-04-13 11:47:21 -0700

    Fix SCP 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,13 +9,13 @@
 
 int main(int argc, char *argv[])
 {
+	int i = 1, mul = 1;
+
 	if (argc < 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	int i = 1, mul = 1;
-
 	while (i < argc)
 	{
 		mul *= atoi(argv[i]);
