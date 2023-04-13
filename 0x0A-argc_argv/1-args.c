19446f7424ed1f0320321825b8ade657f069f99b
Y-Baker <yuossefahmedgaber@gmail.com>
2023-04-13 09:04:27 -0700

    Add SCP 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,28 +1,14 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * main - Prints the number of arguments passed into it
- * @argc: Argument count
- * @argv: Argument vector
- * Return: Always zero
- */
+ * main - print number of input arguments
+ * @argc: Number of command line arguments
+ * @argv: Array of arguments
+ * Return: Always 0
+*/
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	int i;
-
-
-	if (argc == 1)
-		printf("%d\n", argc - 1);
-
-	else
-	{
-		for (i = 0; *argv; i++, argv++)
-			;
-
-		printf("%d\n", i - 1);
-	}
-
+	printf("%d\n", argc - 1);
 	return (0);
 }
