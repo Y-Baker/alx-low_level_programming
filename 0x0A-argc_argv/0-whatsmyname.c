5d20a418bb4f3555b15de50fa9d51896db24abcd
Y-Baker <yuossefahmedgaber@gmail.com>
2023-04-13 08:20:36 -0700

    Add SCP 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,9 +4,10 @@
  * main - write the program name
  * @argc: Number of command line arguments
  * @argv: Array of arguments
+ * Return: Always 0
 */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
