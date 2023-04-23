52f6fc05b04089729a455df6b282c4b2d46f83bd
Y-Baker <yuossefahmedgaber@gmail.com>
2023-04-23 10:00:26 -0700

    Add File 4'

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,13 +1,6 @@
 #ifndef SUM_H
 #define SUM_H
 
-/**
- * File - a macro that adds two integers
- * Desc: Add two integer
- * @x: first integer
- * @y: second integer
- */
-
 #define SUM(x, y) ((x) + (y))
 
 #endif
