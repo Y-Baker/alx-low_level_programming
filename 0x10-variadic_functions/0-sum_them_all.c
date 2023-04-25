63516d16150c4dc2429f5eebae72f2135179cbdb
Youssef <yuossefbakier@gmail.com>
2023-04-25 05:03:39 -0700

    Add File 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,26 +1,24 @@
 #include "variadic_functions.h"
 
 /**
- * sum_them_all - Returns the sum of all its parameters.
- * @n: The number of parameters passed to the function.
- * @...: A variable number of parametes to calculate the sum of.
- * Return: If n == 0 - 0
- * Otherwise - the sum of all parameters
- */
+ * sum_them_all - sum of all input parameter
+ * @n: the number of parameters
+ * Return: the sum of input parameters
+*/
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list nums;
-	unsigned int index, sum = 0;
-
 	if (n == 0)
 		return (0);
+	unsigned int i = 0, sum = 0;
+	va_list nums;
 
 	va_start(nums, n);
-
-	for (index = 0; index < n; index++)
+	while (i < n)
+	{
 		sum += va_arg(nums, int);
-
+		i++;
+	}
 	va_end(nums);
 	return (sum);
 }
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -1,2 +1,1 @@
-# C VARIADIC FUNCTION
--
+[![Typing SVG](https://readme-typing-svg.herokuapp.com?font=Fira+Code&weight=900&size=29&pause=1000&width=435&lines=VARIADIC+FUNCTION!)](https://git.io/typing-svg)
