d9303da4c604a2473912ccc4ee2a296246d32d50
Y-Baker <yuossefahmedgaber@gmail.com>
2023-04-28 01:53:30 +0300

    Fix Betty:

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -4,6 +4,7 @@
  * add_node_end - add node to the end of the list
  * @head: the pointer to the head of the list
  * @str: the string
+ * Return: the new list
 */
 
 list_t *add_node_end(list_t **head, const char *str)
@@ -11,13 +12,14 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new, *last;
 	size_t len = 0;
 	char *strcp;
+
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 	strcp = strdup(str);
 	if (strcp)
 	{
-		free (new);
+		free(new);
 		return (NULL);
 	}
 	while (strcp[len])
@@ -26,7 +28,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	new->str = strcp;
 	new->next = NULL;
 	last = *head;
-	if (*(head) = NULL)
+	if (*(head) == NULL)
 	{
 		*head = new;
 	}
