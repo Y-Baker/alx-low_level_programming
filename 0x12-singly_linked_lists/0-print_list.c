dd3089cc6e0fc76f850ee6d2287694971abef83f
Y-Baker <yuossefahmedgaber@gmail.com>
2023-04-27 22:41:52 +0200

    Fix Taps

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,31 +9,31 @@
 size_t print_list(const list_t *h)
 {
 	size_t count = 0, index = 0;
-    char *str;
+	char *str;
 	while (h)
 	{
 		if (h->str == NULL)
-        {
-            str = "[0] (nil)\n";
-            while (str)
-            {
-                _putchar(str[0]);
-                str++;
-            }
-        }
+		{
+			str = "[0] (nil)\n";
+			while (str)
+			{
+				_putchar(str[0]);
+				str++;
+			}
+		}
 		else
-        {
-            _putchar('[');
-            putchar(h->len + '0');
-            _putchar(']');
-            _putchar(' ');
-            while(index < h->len)
-            {
-                _putchar(h->str[index]);
-                index++;
-            }
-            _putchar('\n');
-        }
+		{
+			_putchar('[');
+			putchar(h->len + '0');
+			_putchar(']');
+			_putchar(' ');
+			while(index < h->len)
+			{
+				_putchar(h->str[index]);
+				index++;
+			}
+			_putchar('\n');
+		}
 		count++;
 		h = h->next;
 	}
