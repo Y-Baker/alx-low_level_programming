2692ad675eb96ca578dd8e9d2b8411b8f788562d
Y-Baker <yuossefahmedgaber@gmail.com>
2023-04-27 22:23:21 +0200

    Update File 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,18 +11,23 @@ size_t print_list(const list_t *h)
     size_t count = 0, index = 0;
     while (h)
     {
-        _putchar('[');
-        putchar(h->len + '0');
-        _putchar(']');
-        _putchar(' ');
-        while(index < h->len)
+        if (h->str)
+            printf("[0] (nil)");
+        else
         {
-            _putchar(h->str[index]);
-            index++;
+            _putchar('[');
+            putchar(h->len + '0');
+            _putchar(']');
+            _putchar(' ');
+            while(index < h->len)
+            {
+                _putchar(h->str[index]);
+                index++;
+            }
         }
-        h = h->next;
-        count++;
         _putchar('\n');
+        count++;
+        h = h->next;
     }
     return (count);
 }
