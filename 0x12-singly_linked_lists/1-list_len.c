9e38cadab1db902657f49757964cdf7944b6a5a7
Y-Baker <yuossefahmedgaber@gmail.com>
2023-04-27 23:08:04 +0200

    Add File 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * list_len - return the length of list
+ * @h: the list
+ * Return: the len of the h
+*/
+
+size_t list_len(const list_t *h)
+{
+    size_t count = 0;
+    while (h)
+    {
+        count++;
+        h = h->next;
+    }
+}
