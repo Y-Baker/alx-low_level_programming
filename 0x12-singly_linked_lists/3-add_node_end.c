a332aca2b22f2c88dbaad815bc3003abc3ba8870
Y-Baker <yuossefahmedgaber@gmail.com>
2023-04-28 01:50:40 +0300

    Add Files

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -8,35 +8,35 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new, *last;
-    size_t len = 0;
-    char *strcp;
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-        return (NULL);
-    strcp = strdup(str);
-    if (strcp)
-    {
-        free (new);
-        return (NULL);
-    }
-    while (strcp[len])
-        len++;
-    new->len = len;
-    new->str = strcp;
-    new->next = NULL;
-    last = *head;
-    if (*(head) = NULL)
-    {
-        *head = new;
-    }
-    else
-    {
-        while (last != NULL)
-        {
-            last = last->next;
-        }
-        last->next = new;
-    }
-    return (*head);
+	list_t *new, *last;
+	size_t len = 0;
+	char *strcp;
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	strcp = strdup(str);
+	if (strcp)
+	{
+		free (new);
+		return (NULL);
+	}
+	while (strcp[len])
+		len++;
+	new->len = len;
+	new->str = strcp;
+	new->next = NULL;
+	last = *head;
+	if (*(head) = NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		while (last != NULL)
+		{
+			last = last->next;
+		}
+		last->next = new;
+	}
+	return (*head);
 }
