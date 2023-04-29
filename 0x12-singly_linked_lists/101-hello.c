83c6865c6fc57bfc20f2888bc1923c2fe8d21074
Y-Baker <yuossefahmedgaber@gmail.com>
2023-04-29 15:08:34 +0300

    Finish Task

---
+++ 0x12-singly_linked_lists/101-hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    char *str = "Hello, Holberton";
+    
+    while (str)
+    {
+        putchar(str[0]);
+        str++;
+    }
+    putchar('\n');
+}
--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,27 +1,41 @@
-; My comments: It is so fun to write in assembly language
-; File: 101-hello_holberton.asm
-; Auth: Stephen Oloo
-; Desc: 64-bit assembly program that prints
-;       Hello, Holberton followed by a new line.
-
-extern printf
-
-section .text
-   global main
-
+	.file	"101-hello.c"
+	.text
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
+.LC0:
+	.ascii "Hello, Holberton\0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
 main:
-   push rbp
-
-   mov rdi,fmt
-   mov rsi,msg
-   mov rax,0
-   call printf
-
-   pop rbp
-
-   mov rax,0
-   ret
-
-section .data
-   msg: db "Hello, Holberton", 0
-   fmt: db "%s", 10, 0
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$48, %rsp
+	.seh_stackalloc	48
+	.seh_endprologue
+	call	__main
+	leaq	.LC0(%rip), %rax
+	movq	%rax, -8(%rbp)
+	jmp	.L2
+.L3:
+	movq	-8(%rbp), %rax
+	movzbl	(%rax), %eax
+	movsbl	%al, %eax
+	movl	%eax, %ecx
+	call	putchar
+	addq	$1, -8(%rbp)
+.L2:
+	cmpq	$0, -8(%rbp)
+	jne	.L3
+	movl	$10, %ecx
+	call	putchar
+	movl	$0, %eax
+	addq	$48, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.ident	"GCC: (GNU) 11.2.0"
+	.def	putchar;	.scl	2;	.type	32;	.endef
