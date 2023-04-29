7c329ee35110740de03970dd3ca26adbaaa9aa10
Y-Baker <yuossefahmedgaber@gmail.com>
2023-04-29 15:40:57 +0300

    Add Last File

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,43 +1,18 @@
-	.file	"101-hello.c"
-	.text
-	.def	__main;	.scl	2;	.type	32;	.endef
-	.section .rdata,"dr"
-.LC0:
-	.ascii "Hello, Holberton\0"
-	.text
-	.globl	main
-	.def	main;	.scl	2;	.type	32;	.endef
-	.seh_proc	main
-main:
-	pushq	%rbp
-	.seh_pushreg	%rbp
-	movq	%rsp, %rbp
-	.seh_setframe	%rbp, 0
-	subq	$48, %rsp
-	.seh_stackalloc	48
-	.seh_endprologue
-	call	__main
-	leaq	.LC0(%rip), %rax
-	movq	%rax, -8(%rbp)
-	jmp	.L2
-.L3:
-	movq	-8(%rbp), %rax
-	movzbl	(%rax), %eax
-	movsbl	%al, %eax
-	movl	%eax, %ecx
-	call	putchar
-	addq	$1, -8(%rbp)
-.L2:
-	movq	-8(%rbp), %rax
-	movzbl	(%rax), %eax
-	testb	%al, %al
-	jne	.L3
-	movl	$10, %ecx
-	call	putchar
-	movl	$0, %eax
-	addq	$48, %rsp
-	popq	%rbp
-	ret
-	.seh_endproc
-	.ident	"GCC: (GNU) 11.2.0"
-	.def	putchar;	.scl	2;	.type	32;	.endef
+section .data
+    message db "Hello, Holberton", 0xA
+
+section .text
+    global _start
+
+_start:
+    ; write message to stdout
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, message
+    mov edx, 16
+    int 0x80
+
+    ; exit
+    mov eax, 1
+    xor ebx, ebx
+    int 0x80
