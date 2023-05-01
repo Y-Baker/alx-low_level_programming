5ce573d23fc5362327c3631949f5cb9191192e0e
Y-Baker <yuossefahmedgaber@gmail.com>
2023-05-01 18:59:35 +0300

    Fix compile error

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *prev;
 	unsigned int i;
 
-	if (!head || idx < 0)
+	if (!head)
 		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (!new)
