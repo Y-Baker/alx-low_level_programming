bb569b875863bc6774e497932da6d2569443b13e
Youssef <yuossefbakier@gmail.com>
2023-05-01 10:52:48 -0700

    Fix compiler error

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,7 +9,7 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	int re = FAILED, count = 0;
+	unsigned int re = FAILED, count = 0;
 	listint_t *last, *next;
 
 	if (!(*head))
