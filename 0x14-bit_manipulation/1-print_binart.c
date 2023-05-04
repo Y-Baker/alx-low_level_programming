d2b903a38f9eb1f23797bc5ccfeaa79699011ec2
Y-Baker <yuossefahmedgaber@gmail.com>
2023-05-05 00:01:40 +0300

    Update Header File

---
+++ 0x14-bit_manipulation/1-print_binart.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_binary - print in binary format
+ * @n: the decimal value
+*/
+
+void print_binary(unsigned long int n)
+{
+	int i, state = 0;
+	unsigned long int present;
+
+	for (i = 31; i >= 0; i--)
+	{
+		present = n >> i;
+
+		if (present & 1)
+		{
+			_putchar('1');
+			state++;
+		}
+		else if (state)
+			_putchar('0');
+	}
+	if (!state)
+		_putchar('0');
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -11,6 +11,7 @@
 					/*prototype*/
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 
 #endif
 
