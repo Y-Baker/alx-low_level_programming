6da9b434643ec0504ed0abde20c55bda127bed58
Youssef <yuossefbakier@gmail.com>
2023-05-04 19:46:24 -0700

    Fix Betty

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- *
+ * get_endianness - check the ends
+ * Return: 0 if large 1 if small
  */
 
 int get_endianness(void)
 {
 	unsigned int x = 1;
-	char *s = (char*) &x;
+	char *s = (char *) &x;
 
-	return ((int)*s);
+	return ((int) *s);
 }
