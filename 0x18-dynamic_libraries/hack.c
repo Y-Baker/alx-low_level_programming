d45453aa0c1c15624feb9c688db89f07a90f59f5
Youssef ( <yuossefbakier@gmail.com>
2023-07-09 06:11:52 -0700

    Finish Hack Sorce Code

--- 0x18-dynamic_libraries/hack.c
+++ 0x18-dynamic_libraries/hack.c
@@ -10,16 +10,16 @@ int rand()
 
 	num++;
 	if (num == 0)
-		return (7);
-	if (num == 1)
-		return (7);
-	if (num == 2)
-		return (6);
-	if (num == 3)
 		return (8);
+	if (num == 1)
+		return (8);
+	if (num == 2)
+		return (7);
+	if (num == 3)
+		return (9);
 	if (num == 4)
-		return (22);
+		return (23);
 	if (num == 5)
-		return (73);
+		return (74);
 	return (num * num % 30000);
 }
