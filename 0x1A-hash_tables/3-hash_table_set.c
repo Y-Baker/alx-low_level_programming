988ec88df01594da86b5f74150a39bd96b809c3d
Youssef ( <yuossefbakier@gmail.com>
2023-07-31 04:00:57 -0700

    Fix Some Bugs

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -24,8 +24,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	tmp = ht->array[idx];
 	while (tmp)
 	{
-		if (strcmp(tmp->key, key))
+		if (strcmp(tmp->key, key) == 0)
+		{
 			tmp->value = (char *)value;
+			return (1);
+		}
 		tmp = tmp->next;
 	}
 
