012e4412c8478e1ec08b97fafffd7406ec1e2a78
Youssef Ahmed <115345602+Y-Baker@users.noreply.github.com>
2023-08-24 01:08:09 +0300

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -10,24 +10,15 @@ def island_perimeter(grid):
     - 0 is water
     - 1 is land
     """
-    num = 0
-    max_width = 0
-    max_height = 0
+    edges = 0
+    size = 0
 
-    for row in grid:
-        for item in row:
-            if item == 1:
-                num += 1
-        if num > max_width:
-            max_width = num
-        num = 0
-
-    for row in range(len(grid[0])):
-        for col in range(len(grid)):
-            if grid[col][row] == 1:
-                num += 1
-        if num > max_height:
-            max_height = num
-        num = 0
-
-    return 2 * (max_width + max_height)
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
